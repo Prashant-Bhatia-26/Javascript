3a7dbe7aeef2a00d1999bc74ee53c953dd673ae7
Prashant-Bhatia-26 <111559911+Prashant-Bhatia-26@users.noreply.github.com>
2022-08-19 18:20:32 +0530

    Add files via upload

---
+++ Javascript/alert.js
@@ -0,0 +1,20 @@
+function demoexternalalert()
+	{
+		alert("external alert!!");
+	}
+	function demoexternalconfirm()
+	{
+		if(confirm("are you surre??"))
+			{
+				alert("yess");
+			}
+		else
+		{
+			alert("noooo");	
+		}
+	}
+	function demoexternalprompt()
+	{
+		var fname=prompt("enter ur name");
+		alert(fname);
+	}
